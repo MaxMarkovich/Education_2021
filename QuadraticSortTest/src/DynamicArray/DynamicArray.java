5215cc2689c5cceadf84d44e1a1dd484adb0ad1a
MaxMarkovich <maaax2002@gmail.com>
2021-09-13 18:37:04 +0300

    Task 5 Complete

---
+++ QuadraticSortTest/src/DynamicArray/DynamicArray.java
@@ -0,0 +1,170 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class DynamicArray<T> {
+    private T[] data;
+    private int DEFAULT_SIZE = 5;
+
+    public static String wrongIndexError = "Index Out of range";
+    public static String negativeArraySizeError = "Size of the array must be over -1";
+    public static String noSuchElementError = "Value not found";
+
+    public DynamicArray() {
+        data = (T[]) new Object[DEFAULT_SIZE];
+    }
+
+    public DynamicArray(int size) {
+        if (size < 0) {
+            throw new RuntimeException(negativeArraySizeError);
+        }
+
+        data = (T[]) new Object[size];
+    }
+
+    public void resize(int newSize) {
+        if (newSize < 0) {
+            throw new RuntimeException(negativeArraySizeError);
+        }
+
+        this.data = Arrays.copyOf(data, newSize);
+    }
+
+    public void setValue(int index, T value) {
+
+        if (index < 0 || index >= data.length) {
+            throw  new RuntimeException(wrongIndexError);
+        }
+
+        data[index] = value;
+    }
+
+    public void removeValue(int index) {
+        // makes new T[] array with one element less
+        // copies all the values to that array until the given index
+        // pops the given index value
+        // appends the rest of the old array to the new array
+        // assigns new array to the old array
+
+        if (index >= data.length || index < 0) {
+            throw new RuntimeException(wrongIndexError);
+        }
+
+        T[] newArray;
+        newArray = (T[]) new Object[data.length - 1];
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = getValue(i);
+        }
+
+        for (int i = index; i < newArray.length; i++) {
+            newArray[i] = getValue(i+1);
+        }
+
+        this.data = newArray;
+    }
+
+    public void insertValue(int index, T value) {
+        // makes new T[] array with one additional empty element
+        // copies all the values to that array until the given index
+        // appends new value by inserting given value to the given index
+        // appends the rest of the old array to the new array
+        // extends old array with 1 empty element
+        // assigns new array to the old array
+
+        if (index >= data.length || index < 0) {
+            throw new RuntimeException(wrongIndexError);
+        }
+
+        T[] newArray;
+        newArray = (T[]) new Object[data.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = getValue(i);
+        }
+
+        newArray[index] = value;
+
+        for (int i = index+1; i < newArray.length; i++) {
+            newArray[i] = getValue(i-1);
+        }
+
+        this.data = newArray;
+    }
+
+    public void addValue(T value) {
+        resize(data.length + 1);
+        setValue(data.length - 1, value);
+    }
+
+    public T getValue(int index) {
+        if (index < 0) {
+            throw new RuntimeException(wrongIndexError);
+        }
+
+        if (index >= data.length) {
+            return null;
+        }
+
+        return data[index];
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    public int findFirst(T requiredElement) {
+        int counter = 0;
+
+        for (T element : this.data) {
+
+            /* -- check if the element is null
+            it is to prevent NullPointerException */
+            boolean notNull = true;
+            if (element == null) {
+                notNull = false;
+            }
+            // -- /check if the element is null
+
+            if (notNull && element.equals(requiredElement)) {
+                return counter;
+            }
+            counter++;
+        }
+
+        throw  new RuntimeException(noSuchElementError);
+    }
+
+    public int findLast(T requiredElement) {
+        int counter = 0;
+        int rightIndex = -1;
+
+        for (T element : this.data) {
+
+            /* -- check if the element is null
+            it is to prevent NullPointerException */
+            boolean notNull = true;
+            if (element == null) {
+                notNull = false;
+            }
+            // -- /check if the element is null
+
+            if (notNull && element.equals(requiredElement)) {
+                rightIndex = counter;
+            }
+            counter++;
+        }
+
+        if (rightIndex < 0) {
+            throw  new RuntimeException(noSuchElementError);
+        }
+
+        return rightIndex;
+    }
+
+    public void printAll() {
+        for (T element : data) {
+            System.out.println(element);
+        }
+    }
+}
---
+++ QuadraticSortTest/src/DynamicArray/DynamicArrayTest.java
@@ -0,0 +1,508 @@
+package Arrays;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class DynamicArrayTests extends Assert {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    //  -- init tests --
+
+    @Test
+    public void init_DefaultArraySize() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        int actualValue = dArr.getSize();
+
+        assertEquals(5, actualValue);
+    }
+
+    @Test
+    public void init_CustomArraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(10);
+
+        int actualValue = dArr.getSize();
+
+        assertEquals(10, actualValue);
+    }
+
+    @Test
+    public void init_CustomArraySize_NegativeSize_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.negativeArraySizeError);
+
+        DynamicArray dArr = new DynamicArray(-1);
+    }
+
+    @Test
+    public void init_CustomArraySize_ZeroSize_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.negativeArraySizeError);
+
+        DynamicArray dArr = new DynamicArray(0);
+    }
+
+    //  -- /init tests --
+
+
+    //  -- set tests --
+
+    @Test
+    public void setValueInt_value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.setValue(1, 69420);
+        Object actualValue = dArr.getValue(1);
+
+        assertEquals(69420, actualValue);
+    }
+
+    @Test
+    public void setValueString_value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.setValue(1, "69420");
+        Object actualValue = dArr.getValue(1);
+
+        assertEquals("69420", actualValue);
+    }
+
+    @Test
+    public void setValue_IndexGreaterThanArrayLength_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.setValue(10, "lala");
+    }
+
+    @Test
+    public void setValue_IndexNegative_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.setValue(-1, "lala");
+    }
+
+    //  -- /set tests --
+
+
+    //  -- resize tests --
+    @Test
+    public void resize_arraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.resize(69420);
+        int actualValue = dArr.getSize();
+
+        assertEquals(69420, actualValue);
+    }
+
+    @Test
+    public void resize_NegativeSize_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.negativeArraySizeError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.resize(-1);
+    }
+
+    @Test
+    public void resize_ZeroSize_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.negativeArraySizeError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.resize(0);
+    }
+
+    //  -- /resize tests --
+
+
+    //  -- get tests --
+
+    @Test
+    public void getValue_value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.setValue(1, 69420);
+        Object actualValue = dArr.getValue(1);
+
+        assertEquals(69420, actualValue);
+    }
+
+    @Test
+    public void getValue_IndexNegative_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.getValue(-1);
+    }
+
+    @Test
+    public void getValue_IndexGreaterThanArraySize_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.getValue(dArr.getSize() + 1);
+    }
+
+    @Test
+    public void getSize_arraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(69420);
+
+        int actualValue = dArr.getSize();
+
+        assertEquals(69420, actualValue);
+    }
+
+    //  -- /get tests --
+
+
+    //  -- findFirst tests --
+
+    @Test
+    public void findFirst_int() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.setValue(1, 69420);
+        int actualValue = dArr.findFirst(69420);
+
+        assertEquals(1, actualValue);
+    }
+
+    @Test
+    public void findFirst_NotExisting_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.noSuchElementError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.findFirst("haha");
+    }
+
+    //  -- /findFirst tests --
+
+
+    //  -- findLast tests --
+
+    @Test
+    public void findLast_int() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.setValue(1, 69420);
+        int actualValue = dArr.findLast(69420);
+
+        assertEquals(1, actualValue);
+    }
+
+    @Test
+    public void findLast_NotExisting_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.noSuchElementError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.findLast("haha");
+    }
+
+    //  -- /findLast tests --
+
+
+    //  -- addValue tests --
+
+    @Test
+    public void addValue_arraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(10);
+
+        dArr.addValue(1);
+        int actualValue = dArr.getSize();
+
+        assertEquals(11, actualValue);
+    }
+
+    @Test
+    public void addValue_Index_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(5);
+
+        dArr.addValue(1);
+        int actualValue = dArr.findFirst(1);
+
+        assertEquals(5, actualValue);
+    }
+
+    @Test
+    public void addValue_Value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(5);
+
+        dArr.addValue(1);
+        Object actualValue = dArr.getValue(5);
+
+        assertEquals(1, actualValue);
+    }
+
+    //  -- /addValue tests --
+
+
+    //  -- insertValue tests --
+
+    @Test
+    public void insertValue_arraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(2, 69420);
+        int actualValue = dArr.getSize();
+
+        assertEquals(7, actualValue);
+    }
+
+    @Test
+    public void insertValue_insertedElementIndex_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(2, 69420);
+        int actualValue = dArr.findFirst(69420);
+
+        assertEquals(2, actualValue);
+    }
+
+    @Test
+    public void insertValue_value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(2, 69420);
+        Object actualValue = dArr.getValue(2);
+
+        assertEquals(69420, actualValue);
+    }
+
+    @Test
+    public void insertValue_NegativeIndex_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(-1, 69420);
+    }
+
+    @Test
+    public void insertValue_IndexGreaterArrayLength_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray(10);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(11, 69420);
+    }
+
+    @Test
+    public void insertValue_IndexEqualsArrayLength_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray(10);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.insertValue(10, 69420);
+    }
+
+    //  -- /insertValue tests --
+
+
+    //  -- removeValue tests --
+
+    @Test
+    public void removeValue_arraySize_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(2);
+        int actualValue = dArr.getSize();
+
+        assertEquals(5, actualValue);
+    }
+
+    @Test
+    public void removeValue_value_Validation() {
+        // positive test
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, 69420);
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(2);
+        Object actualValue = dArr.getValue(2);
+
+        assertEquals(2, actualValue);
+    }
+
+    @Test
+    public void removeValue_removedElementIndex_Validation() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.noSuchElementError);
+
+        DynamicArray dArr = new DynamicArray(6);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(2);
+        int actualValue = dArr.findFirst(69420);
+    }
+
+    @Test
+    public void removeValue_NegativeIndex_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray();
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(-1);
+    }
+
+    @Test
+    public void removeValue_IndexGreaterArrayLength_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray(10);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(11);
+    }
+
+    @Test
+    public void removeValue_IndexEqualsArrayLength_ThrowsException() throws RuntimeException {
+        // negative test
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(DynamicArray.wrongIndexError);
+
+        DynamicArray dArr = new DynamicArray(10);
+        dArr.setValue(0, "garbage");
+        dArr.setValue(1, "garbage");
+        dArr.setValue(2, "gotcha");
+        dArr.setValue(3, 2);
+        dArr.setValue(4, "garbage");
+
+        dArr.removeValue(10);
+    }
+
+    //  -- /removeValue tests --
+}
