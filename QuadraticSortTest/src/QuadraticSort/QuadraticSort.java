56c448a491b8b1416e33c30806ea1bc7b090bd80
MaxMarkovich <maaax2002@gmail.com>
2021-09-13 18:32:44 +0300

    Task 7 Complete

---
+++ QuadraticSortTest/src/QuadraticSort/QuadraticSort.java
@@ -0,0 +1,25 @@
+package QuadraticSort;
+
+import java.util.Arrays;
+
+public class QuadraticSort {
+    public void sort(int[] data)       // Arrays.sort(data); // Replace this
+    {
+        for (int i =0; i < data.length; i++)
+        {
+            int min = data[i];
+            int imin = i;
+            for (int j = i + 1; j < data.length; j++)
+            {
+                if (data[j] < min)
+                {
+                    min = data[j];
+                    imin = j;
+                }
+            }
+            int N = data[i];
+            data[i] = min;
+            data[imin] = N;
+        }
+    }
+}
